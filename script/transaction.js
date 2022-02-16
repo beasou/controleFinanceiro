e8219783d1edcae31cb9f3573a35ee74778f2df0
beasou <beatrizreginasouza1@gmail.com>
2022-02-16 17:29:55 -0300

    formataçao do valor em reais R$ , 00

--- script/transaction.js
+++ script/transaction.js
@@ -41,9 +41,13 @@ const DOM = {
     },
 
     innerHTMLTransaction(transaction){
+        const CSSclass = transaction.amount > 0 ? "income" : "expense"
+        
+        const amount = Utils.formatCurrency(transaction.amount) //pega o valor da formatação da moeda
+        
         const html = `
             <td class="description">${transaction.description}</td>
-            <td class="expense">${transaction.amount}</td>
+            <td class="${CSSclass}">${amount}</td> 
             <td class="date">${transaction.date}</td>
             <td>
                 <img src="./assets/minus.svg" alt="Remover transação">
@@ -53,4 +57,23 @@ const DOM = {
     }
 }
 
-DOM.addTransaction(transactions[0])
+const Utils = { //formatação do valor na tabela
+    formatCurrency(value){
+        const signal = Number(value) < 0 ? "-" : "" //se o valor for menor que 0 atribui sinal de negativio
+        
+        value = String(value).replace(/\D/g, "") //vai buscar globalmente 
+        
+        value = Number(value) / 100
+        
+        value = value.toLocaleString("pt-BR",{ //tipo de formatação de moeda
+            style: "currency",
+            currency: "BRL"
+        })
+        
+        return (signal + value) //retorna o resultado para o amount
+    }
+}
+
+transactions.forEach(function(transaction){
+    DOM.addTransaction(transaction)
+})
